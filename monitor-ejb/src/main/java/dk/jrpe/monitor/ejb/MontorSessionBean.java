7a3c7fe41f84ce78fc45e4dfc93f79232f4be80b
Jörgen Persson <prn@kmd.dk>
2015-06-10 08:46:48 +0200

    Added EJB module

---
+++ monitor-ejb/src/main/java/dk/jrpe/monitor/ejb/MontorSessionBean.java
@@ -0,0 +1,15 @@
+package dk.jrpe.monitor.ejb;
+
+import dk.jrpe.monitor.ejb.remote.MontorSessionBeanRemote;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Jörgen Persson
+ */
+@Stateless
+public class MontorSessionBean implements MontorSessionBeanRemote, MontorSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ montor-ejb-remote/src/main/java/dk/jrpe/monitor/ejb/remote/MontorSessionBeanRemote.java
@@ -0,0 +1,12 @@
+package dk.jrpe.monitor.ejb.remote;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Jörgen Persson
+ */
+@Remote
+public interface MontorSessionBeanRemote {
+    
+}
