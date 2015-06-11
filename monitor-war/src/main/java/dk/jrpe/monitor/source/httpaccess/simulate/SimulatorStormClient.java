c65bc03e29a6e82757e253ded160573b7e7fbb6e
Jörgen Persson <prn@kmd.dk>
2015-06-11 11:27:42 +0200

    Format code

--- monitor-war/src/main/java/dk/jrpe/monitor/source/httpaccess/simulate/SimulatorStormClient.java
+++ monitor-war/src/main/java/dk/jrpe/monitor/source/httpaccess/simulate/SimulatorStormClient.java
@@ -13,10 +13,16 @@ public class SimulatorStormClient {
     public static void main(String[] args) {
         TopologyBuilder builder = new TopologyBuilder();
 
-        builder.setSpout("HTTPAccessSource", new HTTPAccessSpout(),2);
-        builder.setBolt("CXF Sync. Web-service destination", new HTTPAccessCXFSyncWebServiceBolt(), 1).shuffleGrouping("HTTPAccessSource");
-        builder.setBolt("CXF Async. Web-service destination", new HTTPAccessCXFAsyncWebServiceBolt(), 1).shuffleGrouping("HTTPAccessSource");
-        builder.setBolt("Web-socket destination", new HTTPAccessWebSocketBolt(), 1).shuffleGrouping("HTTPAccessSource");
+        builder.setSpout("HTTPAccessSource", new HTTPAccessSpout(),10);
+        
+        builder.setBolt("CXF Sync. Web-service destination", new HTTPAccessCXFSyncWebServiceBolt(), 5)
+        		.shuffleGrouping("HTTPAccessSource");
+        
+        builder.setBolt("CXF Async. Web-service destination", new HTTPAccessCXFAsyncWebServiceBolt(), 5)
+        		.shuffleGrouping("HTTPAccessSource");
+        
+        builder.setBolt("Web-socket destination", new HTTPAccessWebSocketBolt(), 5)
+        		.shuffleGrouping("HTTPAccessSource");
 
         Config conf = new Config();
         conf.setNumWorkers(1);
